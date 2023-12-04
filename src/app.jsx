b2f6455b020a076707da443c16f81e34f2c0efa6
unknown <samtisme@gmail.com>
2023-12-04 12:59:54 -0800

    added app jsx file for app structure component

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./index.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import React from 'react'
+
+function App() {
+    return (
+      <>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  }
+  
+  export default App;
