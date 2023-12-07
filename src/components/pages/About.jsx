90c0e8a76f19df72559c8e230a9683ae4bd890aa
unknown <samtisme@gmail.com>
2023-12-07 11:46:46 -0800

    added about page component

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="about-us-section">
+      <h2 className="section-title">About BB's Smack Shack</h2>
+      <p className="section-description">
+        Welcome to BB's Smack Shack, where passion for seafood meets culinary excellence. Since our establishment,
+        we've been dedicated to providing an unparalleled dining experience, crafting dishes that leave a lasting
+        impression on your taste buds.
+      </p>
+
+      <p>
+        At BB's Smack Shack, we believe in the freshest ingredients and a commitment to quality. Our chefs
+        artfully blend flavors to create mouthwatering seafood dishes that cater to the diverse palates of our
+        valued customers.
+      </p>
+
+      <p>
+        What sets us apart is not just our delectable menu but also our dedication to exceptional service. Whether
+        you're joining us for a casual dining experience or entrusting us with your special events through our
+        catering services, we aim to make every moment memorable.
+      </p>
+
+      <p>
+        Sustainability is at the heart of our operations. We source our seafood responsibly, ensuring that every
+        dish reflects our commitment to the environment and the communities we serve.
+      </p>
+
+      <p>
+        Join us on a culinary journey where every bite is a celebration of flavors. We invite you to explore our
+        menu, savor the freshness, and become a part of the BB's Smack Shack community.
+      </p>
+
+      <p className="contact-info">
+        For inquiries or reservations, please contact us at{' '}
+        <a href="tel:+1234567890">123-456-7890</a> or{' '}
+        <a href="mailto:info@bbsmackshack.com">info@bbsmackshack.com</a>.
+      </p>
+    </section>
+  );
+};
+
+export default About;
