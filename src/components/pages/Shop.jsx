3f3dd7467c53eae05ba555184d058a2e90a9a66e
unknown <samtisme@gmail.com>
2023-12-07 11:41:58 -0800

    finished home page component

---
+++ src/components/pages/Shop.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <section>
+            <h1>
+                Food You'll love brought by people who care.
+            </h1>
+            <div>
+                <p>Why BB's? Simple; we will cater your hearts desires, from jeffs mouth watering barbecue sauce and steak, to amber heart warming passole. The possbilities are endless for your events needs.</p>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <h2>What we offer.</h2>
+                    </li>
+                    <li>
+                        <h3>Barbecue meats.</h3>
+                        <p>No wood fire, Brought to you fresh and juicy we have: Steak, Ribs, Pork, and Sausages.</p>
+                    </li>
+                    <li>
+                        <h3>Seafood</h3>
+                        <p>From Crab, to shrimp and beyond, we will bring you fresh fish and sauces seasonings to spice up your event. Some of our included fish offered are: salmon, cod, tuna, oysters, muscles. If you dont see your fish here ask us ! </p>
+                    </li>
+                    <li>
+                        <h3>Tacos</h3>
+                        <p> tacos made with fresh local ingredients: veggies, protien of your choice, and cheese/sauces.</p>
+                    </li>
+                    <li>
+                        <h3>AND MORE!</h3>
+                        <p>BBs Smack Shack lives to make catering events lively and fun with a food service you can count on, if you have a request you dont think we offer speak with us and inquire, we aim to meet your food needs !</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+}
